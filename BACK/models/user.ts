9104db1ee60689522907d04c3f76eda9620a2855
Robert Vitoriano <robertvitoriano@gmail.com>
2022-08-14 18:31:24 -0300

    add migrations

---
+++ BACK/models/user.ts
@@ -0,0 +1,58 @@
+'use strict';
+import { Model } from 'sequelize';
+
+type UserTributes = {
+  id:string
+  name:string
+  password:string
+  email:string
+  username:string
+}
+
+export default (sequelize:any, DataTypes:any) => {
+  class User extends Model<UserTributes> implements UserTributes {
+    id!: string;
+    name!: string;
+    password!: string;
+    email!: string;
+    username!: string;
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models:any) {
+      // define association here
+    }
+  }
+  User.init({
+    id: {
+      type:DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false
+    },
+    name: {
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    username: {
+      type:DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type:DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
