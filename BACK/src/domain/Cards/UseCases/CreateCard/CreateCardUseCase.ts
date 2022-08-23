515281845cdd78bbe22bbf19c5a261602b8a9448
Robert Vitoriano <robertvitoriano@gmail.com>
2022-08-23 20:07:52 -0300

    add cors and fix token receivement on frontend

--- BACK/src/domain/Cards/UseCases/CreateCard/CreateCardUseCase.ts
+++ BACK/src/domain/Cards/UseCases/CreateCard/CreateCardUseCase.ts
@@ -3,7 +3,6 @@ import { CardAttributes as Card } from "../../../../../models/card"
 import { ICreateCardDTO } from "../../DTOS/ICreateCardDTO";
 export class CreateCardUseCase {
   constructor(private cardsRepository:ICardsRepository){}
-
   async execute(data:ICreateCardDTO):Promise<Card>{
     return this.cardsRepository.createCard(data)
   }
--- BACK/src/infra/http/server.ts
+++ BACK/src/infra/http/server.ts
@@ -2,11 +2,12 @@ import express, { json, Request, Response } from 'express';
 import dotenv from "dotenv"
 import db from './../../../models'
 import {routes} from './routes'
+import cors from 'cors'
 dotenv.config();
 
 const app = express()
 const port = process.env.NODE_LOCAL_PORT || 4444
-
+app.use(cors())
 app.use(json())
 app.use(routes)
 app.get('/',(request:Request, response:Response)=>{
