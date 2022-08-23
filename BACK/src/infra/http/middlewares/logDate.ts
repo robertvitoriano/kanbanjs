c3acc510edb20a275a6ba413beb4af3eca4f75f5
Robert Vitoriano <robertvitoriano@gmail.com>
2022-08-23 19:31:01 -0300

    finish middleware to log date on delete or update a card

--- BACK/src/infra/http/middlewares/logDate.ts
+++ BACK/src/infra/http/middlewares/logDate.ts
@@ -1,10 +1,20 @@
 import { Response, Request, NextFunction } from 'express';
 import { format } from 'date-fns'
-
+import db from './../../../../models'
 export async function logDate(req: Request, res: Response, next: NextFunction): Promise<void> {
   try {
-    const currentDate = format(new Date(), 'dd/MM/yyyy - H:m:ss')
-    // console.log(currentDate)
+    const currentDate = format(new Date(), 'dd/MM/yyyy - H:mm:ss')
+    if (req.route.path === "/cards/:id") {
+      const card = await db.card.findOne({ where: { id: req.params.id } })
+      if (card) {
+        switch (req.method) {
+          case 'DELETE':
+            console.log(`${currentDate} - ${card.titulo} - Removido`)
+          case 'PUT':
+            console.log(`${currentDate} - ${card.titulo} - Alterado`)
+        }
+      }
+    }
     next()
   } catch (error) {
     console.error(error)
