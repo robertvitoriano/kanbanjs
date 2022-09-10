c79f9d8c029df8c1b9a992caae0ea0c54683e995
Robert Vitoriano <robertvitoriano@gmail.com>
2022-09-10 09:09:43 -0300

    improve migraiton runner abstraction

--- BACK/migrations/20220910114728-translate-column-names-of-table-cards.js
+++ BACK/migrations/20220910114728-translate-column-names-of-table-cards.js
@@ -2,15 +2,20 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    
+    await queryInterface.changeColumn("cards","lista",{
+      name:'list',
+    })
+    await queryInterface.changeColumn("cards","conteudo",{
+      name:'content',
+    })
+    await queryInterface.changeColumn("cards","titulo",{
+      name:'title',
+    })
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.changeColumn("cards","list",{
+      name:'lista',
+    })
   }
 };
