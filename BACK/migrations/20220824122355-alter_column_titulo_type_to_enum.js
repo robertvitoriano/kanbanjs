389b29a1f6104415fd721b54341dda941891b541
Robert Vitoriano <robertvitoriano@gmail.com>
2022-09-10 09:26:39 -0300

    fix migrations

--- BACK/migrations/20220824122355-alter_column_titulo_type_to_enum.js
+++ BACK/migrations/20220824122355-alter_column_titulo_type_to_enum.js
@@ -2,13 +2,13 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-   await queryInterface.changeColumn("cards","lista",{
+   await queryInterface.changeColumn("cards","list",{
       type:Sequelize.ENUM("ToDo","Doing","Done", "Blocked"),
     })
   },
 
   async down (queryInterface, Sequelize) {
-   await queryInterface.changeColumn("cards","lista",{
+   await queryInterface.changeColumn("cards","list",{
       type:Sequelize.STRING,
     })
   }
--- BACK/migrations/20220910114728-translate-column-names-of-table-cards.js
+++ BACK/migrations/20220910114728-translate-column-names-of-table-cards.js
@@ -2,20 +2,13 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.changeColumn("cards","lista",{
-      name:'list',
+    await queryInterface.changeColumn("cards","list",{
+      name:'listaa',
     })
-    await queryInterface.changeColumn("cards","conteudo",{
-      name:'content',
-    })
-    await queryInterface.changeColumn("cards","titulo",{
-      name:'title',
-    })
+
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.changeColumn("cards","list",{
-      name:'lista',
-    })
+
   }
 };
